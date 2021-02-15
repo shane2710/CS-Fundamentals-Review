1080b831023ddb1a27ae14912666cd3a1ad0735d
Shane Ryan <sryan8@nd.edu>
2021-02-15 16:48:23 -0500

    added bst deletion

--- data-structures/binary_search_tree/bst.py
+++ data-structures/binary_search_tree/bst.py
@@ -23,6 +23,50 @@ class Node:
             else:
                 self.right = Node(data)
 
+    def findMin(self):
+        node = self
+        while (node.left):
+            node = node.left
+        return node
+
+    def delete(self, data, parent=None):
+        # search for appropriate node
+        if (data < self.data):
+            if (self.left):
+                self.left.delete(data, self)
+                return
+        if (data > self.data):
+            if (self.right):
+                self.right.delete(data, self)
+                return
+        # arrived at appropriate node, perform deletion
+        if (self.left and self.right):
+            # most complex deletion case, need to swap in next node
+            # either pre-order or post-order
+            successor = self.right.findMin()
+            self.data = successor.data
+            successor.delete(successor.data, self)
+        elif (self.left):
+            self.data = self.left.data
+            self.left.delete(self.left.data, self)
+        elif (self.right):
+            self.data = self.right.data
+            self.right.delete(self.right.data, self)
+        else:
+            if (parent):
+            # not removing root node
+                if (parent.left and parent.left.data == self.data):
+                    parent.left = None
+                if (parent.right and parent.right.data == self.data):
+                    parent.right = None
+                del(self)
+            else:
+            # removing root node
+            ## TODO: this doesn't seem to effectively remove root node..?
+                del(self)
+
+
+
     def traverseInOrder(self):
         if (self.left):
             self.left.traverseInOrder()
@@ -38,6 +82,24 @@ bst.insert(3)
 bst.insert(8)
 bst.insert(6)
 bst.insert(9)
+
+print("Initial BST:")
+bst.traverseInOrder()
+
+bst.delete(8)
+bst.insert(8)
+bst.delete(4)
+bst.delete(5)
+bst.delete(9)
+
+print("Modified BST:")
+bst.traverseInOrder()
+
+print("Emptied BST:")
+bst.delete(3)
+bst.delete(7)
+bst.delete(8)
+bst.delete(6)
 bst.traverseInOrder()
 
 
